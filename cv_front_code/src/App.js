c584a4803720f5f1b59d44c9199f573bb593a2f5
MiquelAbellaG <miquel.abella.dw@immune.institute>
2023-03-29 12:08:42 +0200

    Creació del projecte. S'eliminen estils. Falten les noves dependències

---
+++ cv_front_code/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>Arranquem el projecte o què?</p>
+    </div>
+  )
+
+}
+
+export default App;
