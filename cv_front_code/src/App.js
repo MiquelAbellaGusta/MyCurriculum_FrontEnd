25e5318a8b0e9bea6607c6f902dcbfd4f7ee4900
Valhalla <mr.miquel@hotmail.com>
2023-05-18 15:54:59 +0200

    canvis css

--- cv_front_code/src/App.js
+++ cv_front_code/src/App.js
@@ -1,25 +1,20 @@
 
-import './App.css';
+import './css/App.css';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="container">
+        <div className="portrait">
+                </div>
         <article>
-          <p>
-            Durante mi experiencia como arqueólogo entré en contacto con la digitalización de datos e imágenes y empecé a sentir curiosidad por las TI, lo que me llevó a orientarme cada vez más por la parte tecnológica. Cuando terminé el grado me quedó dentro esa curiosidad así que decidí orientar mi vocación a la informática.
-          </p>
-          <p>Ahora soy web developer full stack.</p>
-
-          <p>Me considero una persona que se adapta a los proyectos, dinámica,
-            resolutiva, estable, con una buena capacidad de aprendizaje y que trabaja muy bien en equipo.
-            Es cierto que soy junior pero por esa misma razón mi capacidad de adaptación a los nuevos modelos de trabajo es muy alta y los conocimientos que tengo adquiridos son muy recientes y actualizados.
-          </p>
-
-          <p>Mi objetivo es dominar la mayor cantidad de códigos de programación y llegar a lo más alto.</p>
-        </article >
-
+          <h1></h1>
+          <h3></h3>
+          <p></p>
+          </article>
+          <button></button>
       </div >
     </BrowserRouter >
   )
